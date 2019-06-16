7c61734240cb5e51a520304f902e5649640229a2
ShahAlamSumon <sacsesumon@gmail.com>
2019-06-16 10:00:11 +0600

    [IMP] Connect to postgres and parse data from database

--- real_estate_project/settings.py
+++ real_estate_project/settings.py
@@ -32,12 +32,15 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'harun_real_estate.apps.HarunRealEstateConfig',
 ]
 
@@ -78,8 +81,11 @@ WSGI_APPLICATION = 'real_estate_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'realestatedb',
+        'USER': 'sumon',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
     }
 }
 
@@ -125,3 +131,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'real_estate_project/static')
 ]
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
