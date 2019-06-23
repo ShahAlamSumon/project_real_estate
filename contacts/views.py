c96cf794e7ed662e873dc78f4de2bed7ec081e89
ShahAlamSumon <sacsesumon@gmail.com>
2019-06-24 03:34:58 +0600

    [IMP] Email feature added

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect
 from django.contrib import messages
+from django.core.mail import send_mail
 from .models import Contact
 
 def contact(request):
@@ -13,8 +14,24 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
 
+        # Check user has made query already
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id,user_id=user_id)
+            if has_contacted:
+                messages.error(request,'You have already made an inquiry for this')
+                return redirect('/listings/' + listing_id)
+
         contact = Contact(listing_id=listing_id,listing=listing,name=name,email=email,
                           phone=phone,message=message,user_id=user_id)
         contact.save()
+        # Send mail
+        # send_mail(
+        #     'Proper listing inquiry',
+        #     'There has been inquiry for '+ listing +'.Sign in for more.',
+        #     'sacsesumon@gmail.com',
+        #     [realtor_email, 'sacsesumon@gmail.com'],
+        #     fail_silently=False
+        # )
         messages.success(request,'Your query has been submitted,a realtor will contact you soon')
         return redirect('/listings/'+listing_id)
